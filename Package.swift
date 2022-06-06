acd67f52d48b34a64dfc269cdcd9cbe4b015e449
Jack Murphy <signal.space@pm.me>
2022-06-06 12:35:12 -0500

    adds spm package file

---
+++ Package.swift
@@ -0,0 +1,25 @@
+/ swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "BeanStandardCyborg",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "StandardCyborgUI",
+            targets: ["StandardCyborgUI"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "StandardCyborgUI",
+            dependencies: []),
+        .testTarget(
+            name: "StandardCyborgUITests",
+            dependencies: ["StandardCyborgUI"]),
+    ]
+)
